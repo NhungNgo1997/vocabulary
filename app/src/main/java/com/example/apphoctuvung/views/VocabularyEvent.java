495ffda14b46ed7a533fba742a0a71fcd4ba285e
mduccc <duc2521997@gmail.com>
2020-05-23 23:00:32 +0700

    impl vocabulary detail

--- app/src/main/java/com/example/apphoctuvung/views/VocabularyEvent.java
+++ app/src/main/java/com/example/apphoctuvung/views/VocabularyEvent.java
@@ -1,5 +1,9 @@
 package com.example.apphoctuvung.views;
 
+import com.example.apphoctuvung.data.model.Vocabulary;
+
 public interface VocabularyEvent {
     void onSpeakPressed(String vocabulary);
+
+    void onItemPressed(Vocabulary vocabulary);
 }
