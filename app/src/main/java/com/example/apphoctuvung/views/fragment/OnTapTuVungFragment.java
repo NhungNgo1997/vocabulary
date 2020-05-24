63f96a33f441a8f07ab9e25304e7876ebc046939
mduccc <duc2521997@gmail.com>
2020-05-24 17:03:56 +0700

    Impl ontap

--- app/src/main/java/com/example/apphoctuvung/views/fragment/OnTapTuVungFragment.java
+++ app/src/main/java/com/example/apphoctuvung/views/fragment/OnTapTuVungFragment.java
@@ -1,6 +1,7 @@
 package com.example.apphoctuvung.views.fragment;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,9 +11,49 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.apphoctuvung.R;
+import com.example.apphoctuvung.data.model.Vocabulary;
+import com.example.apphoctuvung.databinding.OntaptuvungFragmentBinding;
+import com.example.apphoctuvung.views.App;
+
+import java.util.List;
+
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.core.SingleObserver;
+import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class OnTapTuVungFragment extends Fragment {
+    private OntaptuvungFragmentBinding binding;
+
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.ontaptuvung_fragment, container, false);
+        binding = OntaptuvungFragmentBinding.bind(inflater.inflate(R.layout.ontaptuvung_fragment, container, false));
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        random();
+    }
+
+    void random() {
+        App.vocabularyRepository.readRandom().subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new SingleObserver<List<Vocabulary>>() {
+                    @Override
+                    public void onSubscribe(@io.reactivex.rxjava3.annotations.NonNull Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onSuccess(@io.reactivex.rxjava3.annotations.NonNull List<Vocabulary> vocabularies) {
+                        Log.d("random",)
+                    }
+
+                    @Override
+                    public void onError(@io.reactivex.rxjava3.annotations.NonNull Throwable e) {
+
+                    }
+                });
     }
 }
