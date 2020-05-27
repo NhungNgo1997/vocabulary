bbf4aa1c8dff8295b3b57bfb9420ba53256ebde6
mduccc <duc2521997@gmail.com>
2020-05-28 00:23:30 +0700

    Fix vocabulary detail

--- app/src/main/java/com/example/apphoctuvung/views/VocabularyDetails.java
+++ app/src/main/java/com/example/apphoctuvung/views/VocabularyDetails.java
@@ -1,16 +1,10 @@
 package com.example.apphoctuvung.views;
 
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.PersistableBundle;
-import android.util.AttributeSet;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.view.inputmethod.EditorInfo;
-import android.widget.Toast;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
@@ -20,48 +14,30 @@ import com.example.apphoctuvung.databinding.VocabularyDetailsBinding;
 import com.example.apphoctuvung.views.adapter.VocabularyDetailsRecyclerAdapter;
 import com.google.gson.Gson;
 
-import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
-import io.reactivex.rxjava3.annotations.NonNull;
-import io.reactivex.rxjava3.core.SingleObserver;
-import io.reactivex.rxjava3.disposables.Disposable;
-import io.reactivex.rxjava3.schedulers.Schedulers;
-
 public class VocabularyDetails extends AppCompatActivity {
     private VocabularyDetailsBinding binding;
-    private Vocabulary listVocabulary;
-    private final  VocabularyDetailsRecyclerAdapter vocabularyDetailsRecyclerAdapter= new VocabularyDetailsRecyclerAdapter();
+    private final VocabularyDetailsRecyclerAdapter vocabularyDetailsRecyclerAdapter = new VocabularyDetailsRecyclerAdapter();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        binding= VocabularyDetailsBinding.inflate(getLayoutInflater());
-        setContentView(R.layout.vocabulary_details);
-         binding.getRoot();
-
-         ds();
-
-
-
-
+        binding = VocabularyDetailsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        ds();
     }
 
 
-
-
     private void ds() {
-        Gson gson= new Gson();
-        Vocabulary vo = gson.fromJson(getIntent().getStringExtra("myjson"),Vocabulary.class);
-        Log.e("MYJSON", getIntent().getStringExtra("myjson"));
-
+        Gson gson = new Gson();
+        Vocabulary vo = gson.fromJson(getIntent().getStringExtra("myjson"), Vocabulary.class);
+        binding.vocabulary.setText(vo.getVocabulary());
         binding.listTranslate.setAdapter(vocabularyDetailsRecyclerAdapter);
         binding.listTranslate.setLayoutManager(new LinearLayoutManager(getParent(), LinearLayoutManager.VERTICAL, false));
-        if (vo!= null && vo.getDetails()!= null && !vo.getDetails().isEmpty()) {
+        if (vo != null && vo.getDetails() != null && !vo.getDetails().isEmpty()) {
             vocabularyDetailsRecyclerAdapter.setDetails(vo.getDetails());
             vocabularyDetailsRecyclerAdapter.notifyDataSetChanged();
         }
     }
-
 
 
 }
